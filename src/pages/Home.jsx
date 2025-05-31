d2c973e87ee30c8415d4cd3315c6b0b6e761d5c6
omowahyu <omowahyu24@gmail.com>
2025-05-31 13:43:10 +0700

    add gap vertical mobile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import ProductFlex from "../components/ProductFlex";
 export default function Home() {
   return (
     <main className="py-12">
-      <div className="grid w-full lg:gap-20">
+      <div className="grid w-full gap-4 lg:gap-20">
       <ProductFlex title="Kitchen" />
       <ProductFlex title="Wardrobe" />
       <ProductFlex title="Somethings" />
